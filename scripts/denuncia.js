360054c9bf3db32edbaa8168558cd5056d73a1b9
Lucas Lima <mosukasangamer@gmail.com>
2025-06-16 21:16:11 -0300

    login, fazer denúncias e ler denúncias

--- scripts/denuncia.js
+++ scripts/denuncia.js
@@ -1,21 +1,24 @@
 import { addDoc, collection } from "@firebase/firestore";
 import { db } from "./firebase";
 
-document.getElementById('').addEventListener('submit', async () => {
-    const denuncia = document.getElementById('').value;
+document.getElementById('form-denuncia').addEventListener('submit', async () => {
+    const denuncia = document.getElementById('txt-denuncia').value;
+    const denunciaStatus = await fazerDenuncia(denuncia);
 
-    await fazerDenuncia(denuncia);
+    if (!denunciaStatus) {
+        alert('Ocorreu um erro ao fazer a denúncia.')
+    }
 });
-
-
 
 async function fazerDenuncia(denuncia) {
     try {
-        const docRef = await addDoc(collection(db, "denuncias"), {
+        await addDoc(collection(db, "denuncias"), {
             denuncia: denuncia
         });
         console.log('Documento escrito com sucesso');
+        return true;
     } catch (e) {
         console.error('Erro ao adicionar o documento: ', e);
+        return false;
     }
 }
--- scripts/login.js
+++ scripts/login.js
@@ -1,8 +1,15 @@
 import { getAuth, signInWithEmailAndPassword} from "firebase/auth";
 
-document.getElementById('').addEventListener('submit', async () => {
-    await signIn();
+document.getElementById('form-login').addEventListener('submit', async () => {
+    const email = document.getElementById('input-email').value;
+    const senha = document.getElementById('input-senha').value;
+    const login = await signIn(email, senha);
     
+    if (login) {
+        window.location.href = '/listaDenuncias.html';
+    } else {
+        alert('Ocorreu um erro ao fazer o login.')
+    }
 });
 
 async function signIn(email, password) {
@@ -10,11 +17,13 @@ async function signIn(email, password) {
     await signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             console.log('Login successfull');
+            return true;
         })
         .catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
 
             console.error('Um erro ocorreu ao fazer o login', `[${errorCode}] ${errorMessage}`);
+            return false;
         });
 }
